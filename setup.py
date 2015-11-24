cfbc19340865794146c67c2a61506100d7cfc07b
Morgan Thrapp <mpthrapp@gmail.com>
2015-11-24 12:19:35 -0500

    Add proper setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyACH',
+    version='1.0',
+    packages=['pyach'],
+    url='https://github.com/morganthrapp/pyACH',
+    license='MIT',
+    author='Morgan Thrapp',
+    author_email='mpthrapp@gmail.com',
+    description='A package to create NACHA files with Python.'
+)
